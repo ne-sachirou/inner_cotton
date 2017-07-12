ade4ff174af68dbf742e32ababacd538db268073
ne_Sachirou <utakata.c4se@gmail.com>
2017-07-12 16:22:13 +0900

    `mix new inner_cotton`

---
+++ lib/inner_cotton.ex
@@ -0,0 +1,18 @@
+defmodule InnerCotton do
+  @moduledoc """
+  Documentation for InnerCotton.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> InnerCotton.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
