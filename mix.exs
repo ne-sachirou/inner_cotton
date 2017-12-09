8cc004e8fc5d0265acf68250ba7ad206686ad9d0
ne_Sachirou <utakata.c4se@gmail.com>
2017-12-10 08:39:16 +0900

    Experimentally add ExCoveralls

--- mix.exs
+++ mix.exs
@@ -13,7 +13,9 @@ defmodule InnerCotton.Mixfile do
       ],
       elixir: "~> 1.5",
       package: package(),
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
       version: "0.2.0",
 
       # Docs
@@ -33,6 +35,7 @@ defmodule InnerCotton.Mixfile do
     [
       {:credo, "~> 0.8", runtime: false},
       {:dialyxir, "~> 0.5", runtime: false},
+      {:excoveralls, "~> 0.7"},
       {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       {:eye_drops, "~> 1.3", runtime: false},
       {:inch_ex, "~> 0.5", runtime: false},
