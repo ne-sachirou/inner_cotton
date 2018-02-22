aad3d2fb905205958f929535dac754f459a69476
ne_Sachirou <utakata.c4se@gmail.com>
2018-02-22 15:25:22 +0900

    Fail `mix cotton.lint` when Dialyzer fails

--- lib/mix/tasks/cotton.lint.ex
+++ lib/mix/tasks/cotton.lint.ex
@@ -21,7 +21,12 @@ defmodule Mix.Tasks.Cotton.Lint do
   def run(_args) do
     Mix.Task.run("format", ["--check-formatted"])
     Mix.Task.run("credo", ["--strict"])
-    Mix.Task.run("dialyzer")
+
+    case Mix.Shell.IO.cmd("mix dialyzer --halt-exit-status") do
+      0 -> nil
+      exit_status -> :erlang.halt(exit_status)
+    end
+
     if Mix.Tasks.Docs in Mix.Task.load_all(), do: Mix.Task.run("inch", ["--pedantic"])
   end
 end
