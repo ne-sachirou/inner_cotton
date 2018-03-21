1f2d809b6a7d759343980ef7ce9dc7e80c55d0ef
ne_Sachirou <utakata.c4se@gmail.com>
2018-03-21 15:56:53 +0900

    Refactor cotton.lint

--- lib/mix/tasks/cotton.lint.ex
+++ lib/mix/tasks/cotton.lint.ex
@@ -26,32 +26,37 @@ defmodule Mix.Tasks.Cotton.Lint do
     fix? = "--fix" in args
     Mix.Task.run("cmd", ["mix do deps.get, compile"])
 
-    [format, credo, dialyzer, inch] =
-      Enum.map(
-        [
-          Task.async(fn ->
-            if fix?, do: Mix.Shell.IO.cmd("mix format --check-equivalent")
-            Mix.Shell.IO.cmd("mix format --check-formatted")
-          end),
-          Task.async(fn -> Mix.Shell.IO.cmd("mix credo --strict") end),
-          Task.async(fn -> Mix.Shell.IO.cmd("mix dialyzer --halt-exit-status") end),
-          Task.async(fn ->
-            if Mix.Tasks.Docs in Mix.Task.load_all(),
-              do: Mix.Shell.IO.cmd("mix inch --pedantic"),
-              else: -1
-          end)
-        ],
-        &Task.await(&1, :infinity)
-      )
+    results =
+      for {name, task} <-
+            [
+              format:
+                Task.async(fn ->
+                  if fix?, do: Mix.Shell.IO.cmd("mix format --check-equivalent")
+                  Mix.Shell.IO.cmd("mix format --check-formatted")
+                end),
+              credo: Task.async(Mix.Shell.IO, :cmd, ["mix credo --strict"]),
+              dialyzer: Task.async(Mix.Shell.IO, :cmd, ["mix dialyzer --halt-exit-status"]),
+              inch:
+                Task.async(fn ->
+                  if Mix.Tasks.Docs in Mix.Task.load_all(),
+                    do: Mix.Shell.IO.cmd("mix inch --pedantic"),
+                    else: -1
+                end)
+            ] do
+        {name, Task.await(task, :infinity)}
+      end
 
-    for {name, status} <- [format: format, credo: credo, dialyzer: dialyzer, inch: inch],
-        status >= 0 do
+    label_length =
+      results |> Keyword.keys() |> Enum.map(&(&1 |> to_string |> String.length())) |> Enum.max()
+
+    for {name, status} <- results, status >= 0 do
       IO.puts(
-        String.pad_trailing(to_string(name), 9) <> ":\t" <> if(0 === status, do: "ok", else: "ng")
+        String.pad_trailing(to_string(name), label_length + 1) <>
+          ":\t" <> if(0 === status, do: "ok", else: "ng")
       )
     end
 
-    case format + credo + dialyzer + max(0, inch) do
+    case results |> Keyword.values() |> Enum.map(&max(&1, 0)) |> Enum.sum() do
       0 -> nil
       exit_status -> :erlang.halt(exit_status)
     end
