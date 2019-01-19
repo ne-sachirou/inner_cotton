90d704bff528131047e849d57341884c79586f9b
ne_Sachirou <utakata.c4se@gmail.com>
2019-01-20 02:29:13 +0900

    Update deps. Drop Elixir 1.6 support.

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule InnerCotton.Mixfile do
         ignore_warnings: "dialyzer.ignore-warnings",
         remove_defaults: [:unknown]
       ],
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       package: package(),
       preferred_cli_env: [
         coveralls: :test,
