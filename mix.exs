2968de66b2322302b526565ba731e41f3bbb5988
ne_Sachirou <utakata.c4se@gmail.com>
2019-07-18 07:15:43 +0900

    v0.5.1
    * Update deps
    * Add Elixir 1.9
    * Drop Elixir 1.7

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule InnerCotton.Mixfile do
       ],
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
-      version: "0.4.1",
+      version: "0.5.1",
 
       # Docs
       docs: [
