5e50dd22cdfa3fae979c2311948e7b1a55855ed3
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-02-19 22:40:12 +0900

    v0.6.1

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule InnerCotton.Mixfile do
       ],
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
-      version: "0.6.0",
+      version: "0.6.1",
 
       # Docs
       docs: [
